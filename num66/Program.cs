aff7cc4b4dbbd83d2833bba4da536c5447aa8cbb
Maxwell11 <maksikbestbest@gmail.com>
2023-07-14 08:32:26 +0300

    dz 9 done

---
+++ num66/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetInt(string message){
+    Console.Write("Введите {0}: " , message);
+    int a = Convert.ToInt32(Console.ReadLine());
+    while( a < 1 ){
+        Console.WriteLine("Элемент должен быть натуральным числом: ");
+        a = Convert.ToInt32(Console.ReadLine());
+    }
+    return a ;
+}
+int Recursion(int m , int n , int result){
+    if (m < n){
+        result = result + m ;
+        return Recursion(m + 1 , n , result);
+    }else if ( m > n){
+        result = result + m ;
+        return Recursion(m - 1 , n , result);
+    }else{
+        return result + m;
+    }
+
+} 
+Console.WriteLine("Полученная сумма :{0}", Recursion(GetInt("M") , GetInt("N") , 0));
---
+++ num68/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetInt(string message){
+    Console.Write("Введите {0}: " , message);
+    int a = Convert.ToInt32(Console.ReadLine());
+    while( a < 0 ){
+        Console.WriteLine("Элемент должен быть неотрицательным числом: ");
+        a = Convert.ToInt32(Console.ReadLine());
+    }
+    return a ;
+}
+int Akkerman(int m , int n){
+    if (m == 0){
+        return n+1;
+    }else if ( m>0 && n==0 ){
+        return Akkerman(m-1 , 1);
+    }else{
+        return Akkerman(m-1, Akkerman(m , n-1));
+    }
+
+} 
+Console.WriteLine("Полученная рекурсия :{0}", Akkerman(GetInt("m") , GetInt("n")));
